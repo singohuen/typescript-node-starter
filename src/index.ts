4c5efd17a282df6456b5ca0f02201914869d6192
Singo Xuan <singohuen@gmail.com>
2020-06-21 00:39:06 +1000

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+import express from "express";
+const app: express.Application = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello world!");
+});
+
+app.listen(8000, () => console.log("app started"));
